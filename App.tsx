e1fd0cec8ab1616d4db03493fa084d89a7bbd2da
Vishal Bhati <vishal3bhati@gmail.com>
2023-07-02 00:47:40 +0530

    Added stack navigator within bottom sheet

--- App.tsx
+++ App.tsx
@@ -5,11 +5,14 @@
  * @format
  */
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React, { useEffect, useRef } from 'react';
-import type { PropsWithChildren } from 'react';
+import BottomSheet from '@gorhom/bottom-sheet';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import type {PropsWithChildren} from 'react';
 import {
+  Pressable,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -18,7 +21,8 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-import { Camera, useCameraDevices } from 'react-native-vision-camera';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {Camera, useCameraDevices} from 'react-native-vision-camera';
 
 import {
   Colors,
@@ -32,7 +36,7 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({ children, title }: SectionProps): JSX.Element {
+function Section({children, title}: SectionProps): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -61,18 +65,81 @@ function Section({ children, title }: SectionProps): JSX.Element {
 const Stack = createNativeStackNavigator();
 
 function LoginScreen(): JSX.Element {
-  return <View style={{ flex: 1, backgroundColor: 'green' }} />
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const snapPoints = useMemo(() => ['50%', '70%'], []);
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+  const [showBottomSheet, setShowBottomSheet] = useState(false);
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'lavender',
+      }}>
+      {showBottomSheet ? (
+        <BottomSheet
+          ref={bottomSheetRef}
+          index={1}
+          snapPoints={snapPoints}
+          onChange={handleSheetChanges}>
+          <Stack.Navigator>
+            <Stack.Screen name="Parent" component={ParentScreen} />
+            <Stack.Screen name="Child" component={ChildScreen} />
+          </Stack.Navigator>
+        </BottomSheet>
+      ) : (
+        <Pressable
+          onPress={() => {
+            setShowBottomSheet(true);
+          }}>
+          <Text style={{fontSize: 24, color: 'black'}}>Show BottomSheet</Text>
+        </Pressable>
+      )}
+    </View>
+  );
+}
+
+function ParentScreen({navigation}): JSX.Element {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Pressable
+        onPress={() => {
+          navigation.navigate('Child');
+        }}>
+        <Text style={{fontSize: 24, color: 'black'}}>Navigate to Child</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+function ChildScreen({navigation}): JSX.Element {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Pressable
+        onPress={() => {
+          navigation.navigate('Parent');
+        }}>
+        <Text style={{fontSize: 24, color: 'black'}}>
+          Navigate back to Parent
+        </Text>
+      </Pressable>
+    </View>
+  );
 }
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 
   return (
